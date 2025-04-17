f6602242f131bb1c42327274af2db46bee23ed1e
lais-sh <lithshamok@gmail.com>
2025-04-17 11:46:53 +1000

    new

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -2,38 +2,43 @@ import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
 document.addEventListener('DOMContentLoaded', () => {
-  const form = document.querySelector('.form');
+    const form = document.querySelector('.form');
 
-  form.addEventListener('submit', evt => {
-    evt.preventDefault();
+    form.addEventListener('submit', evt => {
+        evt.preventDefault();
 
-    const delay = Number(form.elements.delay.value);
-    const state = form.elements.state.value;
+        const delay = Number(form.elements.delay.value);
+        const state = form.elements.state.value;
 
-    const promise = new Promise((resolve, reject) => {
-      setTimeout(() => {
-        if (state === 'fulfilled') {
-          resolve(delay);
-        } else {
-          reject(delay);
-        }
-      }, delay);
-    });
+        const promise = new Promise((resolve, reject) => {
+            setTimeout(() => {
+                if(state === 'fulfilled'){
+                    resolve(delay);
+                } else {
+                    reject(delay);
+                }
+
+            }, delay);
+        });
+    
 
     promise
-      .then(ms => {
-        iziToast.success({
-          message: `✅ Fulfilled promise in ${ms}ms`,
-          position: 'topRight',
+        .then(ms => {
+            iziToast.success({
+                message: ` ✅ Fulfilled promise in ${ms}ms`,
+                position: 'topRight', 
+            });
+
+        })
+        .catch(ms => {
+            iziToast.error({
+                message: `❌ Rejected promise in ${delay}ms`,
+                position: 'topRight',
+            });
         });
-      })
-      .catch(ms => {
-        iziToast.error({
-          message: `❌ Rejected promise in ${ms}ms`, // ← исправлено с delay → ms
-          position: 'topRight',
-        });
-      });
 
     form.reset();
-  });
+
 });
+});
+
