98c9e622255128c9022fd21d5f23a0e1e207eb10
lais-sh <lithshamok@gmail.com>
2025-04-17 11:07:10 +1000

    new

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,44 @@
+import iziToast from "izitoast";   
+import "izitoast/dist/css/iziToast.min.css";
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.querySelector('.form');
+
+    form.addEventListener('submit', evt => {
+        evt.preventDefault();
+
+        const delay = Number(form.elements.delay.value);
+        const state = form.elements.state.value;
+
+        const promise = new Promise((resolve, reject) => {
+            setTimeout(() => {
+                if(state === 'fulfilled'){
+                    resolve(delay);
+                } else {
+                    reject(delay);
+                }
+
+            }, delay);
+        });
+    
+
+    promise
+        .then(ms => {
+            iziToast.success({
+                message: ` ✅ Fulfilled promise in ${ms}ms`,
+                position: 'topRight', 
+            });
+
+        })
+        .catch(ms => {
+            iziToast.error({
+                message: `❌ Rejected promise in ${delay}ms`,
+                position: 'topRight',
+            });
+        });
+
+    form.reset();
+
+});
+});
